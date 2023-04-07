0f8beed8abf0226a7a17e3f348f13105485703cf
Sylvain Montessuit <sylvain.montessuit@gmail.com>
2023-04-07 11:48:34 +0200

    [ADDED] color palette file

---
+++ src/services/palettes.js
@@ -0,0 +1,14 @@
+export const SNES = {
+    lightPurple: "#a7a4e0",
+    darkPurlple: "#514689",
+    lightGrey: "#b2b4b2",
+    darkGrey: "#54585a",
+    grey: "#707372"
+}
+
+export const buttons = {
+    red : "#eb1a1d",
+    green : "#008d45",
+    yellow : "#fece15",
+    blue: "#0749b4"
+}
